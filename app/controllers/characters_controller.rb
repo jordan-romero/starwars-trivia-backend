550b89c6016f736c5191548c1f73135476edf227
MMcClure11 <mcclme02@luther.edu>
2020-09-14 13:45:07 -0500

    adds create action for character

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -3,4 +3,19 @@ class CharactersController < ApplicationController
         characters = Character.all 
         render json: CharacterSerializer.new(characters).to_serialized_json 
     end 
+
+    def create
+        character = Character.new(character_params)
+        if character.save
+            render json: CharacterSerializer.new(character).to_serialized_json 
+        else
+            render json: "Could not save Character"
+        end
+    end
+
+    private
+
+    def character_params
+        params.require(:character).permit(:name, :species, :homeworld, :avatar)
+    end
 end
